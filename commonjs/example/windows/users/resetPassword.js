eff10c32131227670fc06bba08b994731a8841f1
jonalter <jonalter+github@gmail.com>
2013-11-13 09:04:16 -0800

    [TIMODOPEN-295] Adding Users.resetPassword method

---
+++ commonjs/example/windows/users/resetPassword.js
@@ -0,0 +1,87 @@
+windowFunctions['Reset Password'] = function (evt) {
+    var win = createWindow();
+    var offset = addBackButton(win);
+    var content = Ti.UI.createScrollView({
+        top: offset + u,
+        contentHeight: 'auto',
+        layout: 'vertical'
+    });
+    win.add(content);
+
+    var token = Ti.UI.createTextField({
+        hintText: 'reset_password_token',
+        top: 10 + u, left: 10 + u, right: 10 + u,
+        height: 40 + u,
+        borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+        autocapitalization: Ti.UI.TEXT_AUTOCAPITALIZATION_NONE,
+        autocorrect: false
+    });
+    content.add(token);
+    
+    var password = Ti.UI.createTextField({
+        hintText: 'password',
+        top: 10 + u, left: 10 + u, right: 10 + u,
+        height: 40 + u,
+        borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+        autocapitalization: Ti.UI.TEXT_AUTOCAPITALIZATION_NONE,
+        autocorrect: false
+    });
+    content.add(password);
+    
+    var passwordConfirmation = Ti.UI.createTextField({
+        hintText: 'password_confirmation',
+        top: 10 + u, left: 10 + u, right: 10 + u,
+        height: 40 + u,
+        borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+        autocapitalization: Ti.UI.TEXT_AUTOCAPITALIZATION_NONE,
+        autocorrect: false
+    });
+    content.add(passwordConfirmation);
+
+    var button = Ti.UI.createButton({
+        title: 'Reset Password',
+        top: 10 + u, left: 10 + u, right: 10 + u, bottom: 10 + u,
+        height: 40 + u
+    });
+    content.add(button);
+
+    var fields = [ token, password, passwordConfirmation ];
+
+    function submitForm() {
+        for (var i = 0; i < fields.length; i++) {
+            if (!fields[i].value.length) {
+                fields[i].focus();
+                return;
+            }
+            fields[i].blur();
+        }
+        button.hide();
+
+        Cloud.Users.resetPassword({
+            reset_password_token: token.value,
+            password: password.value,
+            password_confirmation: passwordConfirmation.value
+        }, function (e) {
+            if (e.success) {
+                alert('Password successfully reset!');
+                token.value = '';
+                password.value = '';
+                passwordConfirmation.value = '';
+            }
+            else {
+                error(e);
+            }
+            button.show();
+        });
+    }
+
+    button.addEventListener('click', submitForm);
+    for (var i = 0; i < fields.length; i++) {
+        fields[i].addEventListener('return', submitForm);
+    }
+
+    win.addEventListener('open', function () {
+        token.focus();
+    });
+    win.open();
+};
